053b9d22f08116094d7c678c6b1f6002b4ccaa94
bl6ndr <75757171+bl6ndr@users.noreply.github.com>
2023-05-27 08:27:31 -0700

    Fix all bugs

--- darkcrypt.py
+++ darkcrypt.py
@@ -1,5 +1,6 @@
 import base64
 import os
+import platform
 import tkinter as tk
 from tkinter import filedialog
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
@@ -9,14 +10,17 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding as rsa_paddin
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-root = tk.Tk()
-root.withdraw()
+def select_trojan_file():
+    root = tk.Tk()
+    root.withdraw()
+    trojan_path = filedialog.askopenfilename(title="Select Trojan File")
+    return trojan_path
 
-trojan_path = filedialog.askopenfilename(title="Select Trojan File")
-encrypted_trojan_path = filedialog.asksaveasfilename(title="Save Encrypted Trojan As", defaultextension='.exe')
-
-with open(trojan_path, 'rb') as file:
-    trojan_data = file.read()
+def select_directory():
+    root = tk.Tk()
+    root.withdraw()
+    directory_path = filedialog.askdirectory(title="Select Directory to Save Encrypted Trojan")
+    return directory_path
 
 def encrypt_aes(data, key):
     padder = padding.PKCS7(128).padder()
@@ -39,39 +43,60 @@ def encrypt_rsa(data, public_key):
     )
     return encrypted_data
 
-salt = b'cGc0zoasYzPYEd7mf18Z9hy7VDfAkpauLRrOxMtrbrskDd4eyoktbegKhmlpc4KHdmEMda0zdevovDCx2HoXMPu9NOvbeAd08ZOy'
-password = b'darktaple'
-kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    iterations=100000,
-    backend=default_backend()
-)
-aes_key = kdf.derive(password)
+def save_encrypted_trojan(data, path):
+    with open(path, 'wb') as file:
+        file.write(data)
 
-rsa_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-    backend=default_backend()
-)
-public_key = rsa_key.public_key()
+def open_directory(path):
+    if platform.system() == 'Windows':
+        os.startfile(path)
+    elif platform.system() == 'Darwin':
+        os.system(f"open {path}")
+    else:
+        os.system(f"xdg-open {path}")
 
-encrypted_data_aes = encrypt_aes(trojan_data, aes_key)
+def encrypt_trojan():
+    trojan_path = select_trojan_file()
+    if not trojan_path:
+        print("No Trojan file selected.")
+        return
 
-encrypted_aes_key = encrypt_rsa(aes_key, public_key)
+    directory_path = select_directory()
+    if not directory_path:
+        print("No directory selected.")
+        return
 
-encrypted_data_base64 = base64.b64encode(encrypted_data_aes).decode('utf-8')
-encrypted_key_base64 = base64.b64encode(encrypted_aes_key).decode('utf-8')
+    with open(trojan_path, 'rb') as file:
+        trojan_data = file.read()
 
-directory_path = filedialog.askdirectory(title="Select Directory to Save Encrypted Trojan")
-encrypted_trojan_path = os.path.join(directory_path, os.path.basename(encrypted_trojan_path))
+    salt = b'cGc0zoasYzPYEd7mf18Z9hy7VDfAkpauLRrOxMtrbrskDd4eyoktbegKhmlpc4KHdmEMda0zdevovDCx2HoXMPu9NOvbeAd08ZOy'
+    password = b'darktaple'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    aes_key = kdf.derive(password)
 
-with open(encrypted_trojan_path, 'wb') as file:
-    file.write(encrypted_data_aes)
+    rsa_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    public_key = rsa_key.public_key()
 
-if platform.system() == 'Windows':
-    os.startfile(directory_path)
-else:
-    opener = "open" if platform.system() == "Darwin" else "xdg-open"
-    os.system(f"{opener} {directory_path}")
+    encrypted_data_aes = encrypt_aes(trojan_data, aes_key)
+    encrypted_aes_key = encrypt_rsa(aes_key, public_key)
+
+    encrypted_data_base64 = base64.b64encode(encrypted_data_aes).decode('utf-8')
+    encrypted_key_base64 = base64.b64encode(encrypted_aes_key).decode('utf-8')
+
+    encrypted_trojan_path = os.path.join(directory_path, os.path.splitext(os.path.basename(trojan_path))[0] + ".exe")
+
+    save_encrypted_trojan(encrypted_data_aes, encrypted_trojan_path)
+    open_directory(directory_path)
+
+if __name__ == '__main__':
+    encrypt_trojan()
